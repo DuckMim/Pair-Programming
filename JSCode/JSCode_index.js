66aa1773bfc609699c185b1404e93772c75de879
Duck Mim <m.sokolenko@l208.kiev.ua>
2025-09-26 09:42:42 +0300

    Update JSCode_index.js

--- JSCode/JSCode_index.js
+++ JSCode/JSCode_index.js
@@ -20,51 +20,9 @@ async function JoinRoom() {
 
     if (res.status != 200) return PopUpWindow(res.description);
 
+    await sessionStorage.clear();
+
     await sessionStorage.setItem("roomCode", joinRoomCodeValue);
     await sessionStorage.setItem("playerIndex", res.playerIndex);
     window.location.href = "playerPage.html";
-}
-
-async function MakeRoom() {
-    let makeRoomCodeValue = MAKE_ROOM_CODE_INPUT.value;
-    let roomPlayersCount = COUNT_OF_PLAYERS_INPUT.value;
-
-    if (makeRoomCodeValue.length < 4) return PopUpWindow("Incorrect code type (must be 4 numbers)");
-    if (roomPlayersCount < 2) return PopUpWindow("Count of players is to small (at least 2)");
-    if (roomPlayersCount > 199) return PopUpWindow("Count of players is to big (at most 199)");
-
-    for (let i = 0; i < makeRoomCodeValue.length; i++) {
-        let charCode = makeRoomCodeValue.charCodeAt(i);
-        if (charCode < 48 || charCode > 57) return PopUpWindow("Incorrect code type (must be 4 numbers)");
-    }
-
-    let res = await SendPost("RoomManager", "MakeRoom", {roomCode: makeRoomCodeValue, maxCountOfPlayers: parseInt(roomPlayersCount)});
-
-    if (res.status != 200) return PopUpWindow(res.description);
-
-    await sessionStorage.setItem("roomCode", makeRoomCodeValue);
-    window.location.href = "adminPage.html";
-}
-
-async function MakeRandomRoomCode() {
-    let currentRoomCode = RandomString(4);
-
-    MAKE_ROOM_CODE_INPUT.value = `${currentRoomCode}`;
-}
-
-function RandomString(length) {
-    let result = '';
-    const CHARACTERS = '0123456789';
-
-    for (let i = 0; i < length; i++) {
-        let randomInd = Math.floor(Math.random() * CHARACTERS.length);
-        result += CHARACTERS.charAt(randomInd);
-    }
-
-    return result;
-}
-
-async function WakeUpServersButton() {
-    await WakeUpServers();
-    return PopUpWindow("Servers are awake!", "./Icons/WakeUpIcon.png");
 }
